e51463a68466e4a3500c35d77c7f2c93f5c29116
Sanad Thapa <sanadtm@gmail.com>
2023-04-19 23:58:58 -0700

    Added Styled Components

--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,20 @@ import HomeContent from "./component/HomeContent";
 import MyProjects from "./component/MyProjects";
 import NavBar from "./component/NavBar";
 import Footer from "./component/Footer";
+import styled from "styled-components";
+const Container = styled.div`
+    height: 100hv;
+    background-color: rebeccapurple;
+`;
 export default function Home() {
     return (
-        <>
+        <Container>
             <NavBar />
             <HomeContent />
             <AboutMe />
             <MyProjects />
             <ContactMe />
             <Footer />
-        </>
+        </Container>
     );
 }
