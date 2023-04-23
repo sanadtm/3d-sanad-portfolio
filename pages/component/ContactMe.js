fd0a9fbd6180b84eb201c606792bcff733ba64fe
Sanad Thapa <sanadtm@gmail.com>
2023-04-22 22:15:43 -0700

    Added Links to reach the Sections

--- pages/component/ContactMe.js
+++ pages/component/ContactMe.js
@@ -8,17 +8,19 @@ export default function ContactMe() {
                     <div className="text-white w-5/6 justify-between  items-center flex ">
                         <div className="gap-10">
                             <div>
-                                <h1 className="text-6xl font-bold  subpixel-antialiased animate-bounce">Contact.Me.</h1>
+                                <h1 className="text-6xl py-2  font-bold  subpixel-antialiased animate-bounce">
+                                    Contact.Me.
+                                </h1>
                             </div>
                             <div className="flex">
-                                <div className="text-4xl font-bold text-yellow-400  subpixel-antialiased">
+                                <div className="text-4xl py-2  font-bold text-yellow-400  subpixel-antialiased">
                                     -What i Do
                                 </div>
                             </div>
-                            <div className="text-lg ont-bold subpixel-antialiased">
+                            <div className="text-lg py-2  ont-bold subpixel-antialiased">
                                 I Enjoy Creating Delightful, human-centered
                             </div>
-                            <div className="text-lg font-bold text-yellow-400  subpixel-antialiased">
+                            <div className="text-lg py-2  font-bold text-yellow-400  subpixel-antialiased">
                                 Digital Experience
                             </div>
                             <button className="transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-1 px-4 border border-white-700 rounded">
--- pages/component/HomeContent.js
+++ pages/component/HomeContent.js
@@ -7,24 +7,24 @@ export default function HomeContent() {
                 <NavBar />
                 <div className="text-white w-6/6 justify-center  items-center flex p-4 flex ">
                     <div className="text-white w-5/6 justify-between  items-center flex ">
-                        <div className="gap-10">
+                        <div className="py-10 ">
                             <div>
-                                <h1 className="text-6xl font-bold  subpixel-antialiased animate-bounce">
+                                <h1 className="text-6xl font-bold py-2 subpixel-antialiased animate-bounce">
                                     Think.Create.Design.
                                 </h1>
                             </div>
                             <div className="flex">
-                                <div className="text-4xl font-bold text-yellow-400  subpixel-antialiased">
+                                <div className="text-4xl font-bold py-2 text-yellow-400  subpixel-antialiased">
                                     -What i Do
                                 </div>
                             </div>
-                            <div className="text-lg ont-bold subpixel-antialiased">
+                            <div className="text-lg ont-bold py-2 subpixel-antialiased">
                                 I Enjoy Creating Delightful, human-centered
                             </div>
-                            <div className="text-lg font-bold text-yellow-400  subpixel-antialiased">
+                            <div className="text-lg py-2 font-bold text-yellow-400  subpixel-antialiased">
                                 Digital Experience
                             </div>
-                            <button className="transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-1 px-4 border border-black-700 rounded">
+                            <button className="transition  ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-1 px-4 border border-black-700 rounded">
                                 Learn More
                             </button>
                         </div>
